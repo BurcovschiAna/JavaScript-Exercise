9b828c470bc1b7c3dab436ced8a1fbe0df6980a7
Ana Burcovschi <ana@burcovschi.com>
2024-08-09 21:29:24 +0300

    Init

--- Drum Kit/script.js
+++ Drum Kit/script.js
@@ -1,24 +1,21 @@
-// const drums = [];
-// for(let i = 1; i <= 6; i++){
-//     drums.push(document.getElementById(`drum-${i}`));
-// }
-// for(let i = 0; i < drums.length; i++){
-//     drums[i].addEventListener("click", function (){
+const drums = [];
+for(let i = 1; i <= 6; i++){
+    drums.push(document.getElementById(`drum-${i}`));
+}
+for(let i = 0; i < drums.length; i++){
+    drums[i].addEventListener("click", function (){
 
-//     });
-// }
+    });
+}
+const sounds = [
+    "./sounds/crash.mp3",
+    "./sounds/snare.mp3",
+    "./sounds/kick-bass.mp3",
+    "./sounds/medium tom.mp3",
+    "./sounds/hi tom.mp3",
+    "./sounds/Floor tom.mp3"
+];
 
-// const sounds = [
-//     "./sounds/crash.mp3",
-//     "./sounds/snare.mp3",
-//     "./sounds/kick-bass.mp3",
-//     "./sounds/medium tom.mp3",
-//     "./sounds/hi tom.mp3",
-//     "./sounds/Floor tom.mp3"
-// ];
+function makeASound(){
 
-// function makeASound(){
-//     for(let i = 0; i < drums.length; i++){
-//         let audion = new Audio(sounds[i]).play().this;
-//     }
-// }
+}
